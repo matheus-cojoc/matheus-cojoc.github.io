570a93cd1be12a1840dd2129b8d00bf7ade92e76
Mateo Cojoc <matheus.cojoc@salis.terra>
2019-02-28 15:24:09 +0200

    bla

---
+++ precache-manifest.797e1108f816f1dbfece54811c797f92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8cd2a8f50a2d77ecdafc",
+    "url": "/static/js/main.8cd2a8f5.chunk.js"
+  },
+  {
+    "revision": "e2b908d7b2020c01bd22",
+    "url": "/static/js/2.e2b908d7.chunk.js"
+  },
+  {
+    "revision": "8cd2a8f50a2d77ecdafc",
+    "url": "/static/css/main.51aa2f99.chunk.css"
+  },
+  {
+    "revision": "cda1fe5ecf42a7f2a89c7ff63063dbd3",
+    "url": "/index.html"
+  }
+];
